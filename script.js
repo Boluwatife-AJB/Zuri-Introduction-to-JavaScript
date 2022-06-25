c8b4687bf7e627ce5fe379e1bf0768a0228c3134
Boluwatife-AJB <56480003+Boluwatife-AJB@users.noreply.github.com>
2022-06-25 01:44:39 +0100

    I added the initial files for this project

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const aboutMe = {
+    firstName: 'Boluwatife',
+    lastName: 'Ajose',
+    height: '1.64 meters',
+    country: 'Nigeria',
+}
+
+alert(
+    `My name is ${aboutMe.firstName} ${aboutMe.lastName}. I am ${aboutMe.height} tall and I am from ${aboutMe.country}.`
+)
